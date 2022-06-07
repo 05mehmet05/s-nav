d42ea74ad96e976ef084f88d1fe20d17d2af4bbe
mehmet ali <90866582+05mehmet05@users.noreply.github.com>
2022-06-07 14:00:59 +0300

    Add files via upload

---
+++ .js
@@ -0,0 +1,17 @@
+
+
+function sıfırla() {
+    button=sıfırla=(onclick=0);
+}
+function oyna() {
+   for(int i = 0; i < 100; i++)consolelog
+
+}
+if (sayı) {
+    oyna>sayı
+} else {
+    =(daha büyük sayı girin)
+}
+    
+
+
